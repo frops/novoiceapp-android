ae120c49feefef8f705ddcfcbe7c785a5ca5c79e
Ildar Asanov <ifrops@gmail.com>
2025-10-17 16:33:47 +0300

    Adopt Expo Router navigation

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
-    browser: true
+    browser: true,
+    jest: true
   },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
   parser: '@typescript-eslint/parser',
